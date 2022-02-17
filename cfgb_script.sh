645c4af4beee14c784c4652a1ca6b9e978be9c05
Matheus Dias <matheus.d.silva112@gmail.com>
2022-02-17 10:03:58 -0300

    Add files via upload

--- cfgb_script.sh
+++ cfgb_script.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 clear
-echo $0
+msg=(
+'Error: this command needs run with sudo: \n Ex: sudo command/script '
+'Error: Enter your repository URL after setup'
+)
+
 u=$(whoami) ; if [ $u != root ]; then echo 'Error: this command needs run with sudo: 
-Ex: sudo command/script ' ; exit ; fi
+Ex: sudo command/script' ; exit ; fi
 
 if [ $1 = setup ]; then
-if [  $2 =   ] ; then echo 'Error: Enter your repository URL after setup ' ; exit ; fi ;
+if [ $2 = "" ] ; then echo  ; exit ; fi ;
 sudo cp -r "$(pwd)/cfgb_script.sh" /bin/cfgb
-sudo mkdir /usr/share/cfgb ; sudo mkdir /usr/share/cfgb/bundles ; 
-sudo echo $2 > /usr/share/cfgb/repo
+sudo mkdir /usr/share/cfgb ; sudo mkdir /usr/share/cfgb/bundles ;
+
+#setting configs
+sudo echo -e "export repo=$3
+export h=/home/$2" > /usr/share/cfgb/cfg ;
 echo C.F.G.B Manager instaled
 exit
 fi
-if [ $1 = update ]; then sudo cp -r "$(pwd)/cfgb_script.sh" /bin/cfgb ; echo C.F.G.B Manager Updated ; exit ; fi
+#if [ $1 = update ]; then sudo cp -r "$(pwd)/cfgb_script.sh" /bin/cfgb ; echo C.F.G.B Manager Updated ; exit ; fi
 
 #Global variables
 glb="export" ; $glb name=cfgb ; $glb cp="sudo cp -r";$glb rm="sudo rm -rf";$glb prt="echo -e" ; $glb pdir=/usr/share/$name ; $glb id=$2 ; $glb mkd='sudo mkdir' ; $glb bnd_dir=$pdir/bundles
 
 #Setup
-repo=$(cat $pdir/repo)
-cd $bnd_dir ; 
+cfg=$(cat $pdir/cfg); $cfg ; cd $bnd_dir ;
 
 #pkg install function
 pkg_install () { 
@@ -34,23 +40,24 @@ export -f pkg_install
 $prt '[Configuration Bundles Manager] by -=Matheus Dias=-
 ~Making desktop setups most simple possible!~
 '
+#args=($2 $3 $4 $5 $6) ; for a in ${args[@]} ; do sudo cfgb -i $a ; done
+if [ $1 = -i ]; then 
+args=($2 $3 $4 $5 $6) ; for a in ${args[@]} ; do
 #getting a bundle
-if [ $1 = -i ]; then
 echo '-=- ['$name']: Download Bundle -=-
 Repository: '$repo''
-wget -q $repo/$2.tar.gz ;
+wget $repo/$a.tar.gz ;
 echo 'tar: ['$(ls $bnd_dir/ )'] -Ok
 '
-
 #unpacking a bundle
-echo '-=- ['$2']: Unpacking Bundle -=-'
-$mkd $2/ ; tar -xf $2.tar.gz -C $2/ ; $rm $2.tar.gz
-echo files: [$(ls -c $bnd_dir/$2)] -Ok
-fi
-
+echo '-=- ['$a']: Unpacking Bundle -=-'
+$mkd $a/ ; tar -xf $a.tar.gz -C $a/ ; $rm $a.tar.gz
+echo files: [$(ls -c $bnd_dir/$a)] -Ok
 
 #Cooking directories recipes
-$prt '-=- ['$2']: Cooking Bundle -=-'
-cd $bnd_dir/$2/ ; bash recipe
-echo '-=- '$2' Instaled -=-' ; 
+$prt '-=- ['$a']: Cooking Bundle -=-'
+cd $bnd_dir/$a/ ; bash recipe
+echo '-=- '$a' Instaled -=-' ;  done ;
+fi
+
 $rm $bnd_dir/*
